e2e3044cf54f92015ef0decaff0bceef48b9a94a
Tomy <dead_forty@hotmail.com>
2025-11-27 21:11:26 -0600

    Se modifico un inconveninte con los endpoints en estos archivos

--- frontend/src/api/admin.js
+++ frontend/src/api/admin.js
@@ -2,31 +2,31 @@ import api from './axios';
 
 // Get all clients with details (subscriptions, status, etc.)
 export const getAdminClients = async () => {
-  const response = await api.get('/admin/clients');
+  const response = await api.get('api/admin/clients');
   return response.data;
 };
 
 // Get dashboard statistics
 export const getAdminStats = async () => {
-  const response = await api.get('/admin/stats');
+  const response = await api.get('api/admin/stats');
   return response.data;
 };
 
 // Get all subscription plans for admin
 export const getAdminSubscriptionPlans = async () => {
-  const response = await api.get('/admin/subscription-plans');
+  const response = await api.get('api/admin/subscription-plans');
   return response.data;
 };
 
 // Toggle user active/inactive status
 export const toggleUserStatus = async (userId) => {
-  const response = await api.put(`/admin/clients/${userId}/toggle-status`);
+  const response = await api.put(`api/admin/clients/${userId}/toggle-status`);
   return response.data;
 };
 
 // Change user subscription plan
 export const changeUserSubscription = async (userId, planId) => {
-  const response = await api.put(`/admin/clients/${userId}/change-subscription`, {
+  const response = await api.put(`api/admin/clients/${userId}/change-subscription`, {
     planId,
   });
   return response.data;
